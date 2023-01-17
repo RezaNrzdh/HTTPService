ecab2448cb916ae275e9941f1caa1795a2545261
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-01-18 01:28:04 +0330

    Create Hello Router

---
+++ route/hello.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    HelloStranger,
+    HelloWithParamsName
+} = require("../controllers/hello.controller");
+
+router.route("/").get(HelloStranger);
+router.route("/:name").get(HelloWithParamsName);
+
+module.exports = router;
