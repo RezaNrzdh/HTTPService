990ccb8725ad4c0b74d30263eeda8b9d6e31e442
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-01-18 01:51:24 +0330

    Add the environment variable to the project.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const dotenv = require("dotenv");
 const app = require("./app");
 
 // Environment Variables
-const PORT = 8080;
+dotenv.config({ path: './config.env' });
+const PORT = process.env.PORT;
 
 // Listening
 app.listen(PORT, console.log("Waiting for request..."));
