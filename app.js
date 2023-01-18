77b75827969bc5aef5f8b2057ea928365dff754a
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-01-18 13:06:05 +0330

    Create new middleware for log.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 // import route module
 const helloRoute  = require("./route/hello.route");
@@ -9,6 +10,7 @@ const app = express();
 
 // app config
 app.use(express.json());
+app.use(morgan(':remote-addr - - [:date[iso]] ":method :url" :status - - :response-time ms'));
 
 // routes
 app.use("/hello/", helloRoute);
